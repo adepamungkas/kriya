3085f960eb0bbbdcee9ce61945d1ee2be458d957
adepamungkas <ade.pamungkas9@gmail.com>
2020-03-19 01:48:26 +0700

    Updates

---
+++ precache-manifest.662f94f76c0115c9708be88346dd47c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eda079871c8990abff6cfdcd0bc028a",
+    "url": "/kriya/index.html"
+  },
+  {
+    "revision": "8f0565bbb823c557ccce",
+    "url": "/kriya/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f52a32533dd6b5dbe56",
+    "url": "/kriya/static/js/2.66a0cf29.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/kriya/static/js/2.66a0cf29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0565bbb823c557ccce",
+    "url": "/kriya/static/js/main.7865483f.chunk.js"
+  },
+  {
+    "revision": "247efc5bf324136ea9be",
+    "url": "/kriya/static/js/runtime-main.4edc2558.js"
+  }
+]);
